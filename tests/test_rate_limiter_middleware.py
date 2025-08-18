436f65ff6ef1fc975d07044cd70e4c0424b51a7b
b-franken <basfrankenn@gmail.com>
2025-08-18 14:01:39 +0200

    chore(api) merged api v2 version into one api

--- tests/test_rate_limiter_middleware.py
+++ tests/test_rate_limiter_middleware.py
@@ -6,17 +6,18 @@ from fastapi.testclient import TestClient
 
 logger = logging.getLogger(__name__)
 
+
 class DummyLimiter:
     async def check_rate_limit(self, request: Request) -> None:  # pragma: no cover - stub
         return None
 
+
 limiter = DummyLimiter()
 app = FastAPI()
 
+
 @app.middleware("http")
-async def _rl_mw(
-    request: Request, call_next: Callable[[Request], Awaitable[Response]]
-) -> Response:
+async def _rl_mw(request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
     await limiter.check_rate_limit(request)
     try:
         return await call_next(request)
@@ -24,9 +25,11 @@ async def _rl_mw(
         logger.exception("Middleware error")
         raise HTTPException(status_code=500, detail="Internal Server Error") from exc
 
+
 @app.get("/error")
 async def _error() -> None:
     raise RuntimeError("boom")
+
 
 client = TestClient(app, raise_server_exceptions=False)
 
